4f5e7c498cbaf2abc70e391590cb6804ee0d3c3c
trienko <trienkog@gmail.com>
2018-06-27 14:27:26 +0200

    First python file submitted

---
+++ Red.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import numpy as np
+import scipy.stats as stats
+import pylab as plt
+import math as mt
+
+class Red(object):
+
+  def __init__(self):
+      pass
+
+
+if __name__ == "__main__":
+        red_object = Red() 
+        
+
+
