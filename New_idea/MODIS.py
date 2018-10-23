f989679c5fe1ecab169b4018b4314c74bc28851a
trienko <trienkog@gmail.com>
2018-10-23 12:14:38 +0200

    started new project

---
+++ New_idea/MODIS.py
@@ -0,0 +1,66 @@
+import scipy.io
+import pylab as plt
+from sklearn.decomposition import PCA
+import numpy as np
+from scipy.fftpack import fft
+import matplotlib.pyplot as plt
+from scipy.optimize import leastsq
+from matplotlib.patches import Ellipse
+from termcolor import colored
+from sklearn import mixture
+
+class MODIS():
+
+  def __init__(self):
+      pass
+
+  def loadDataSet(self,name="Gauteng_nochange.mat",province="Gauteng"):
+      print("Loading Dataset")
+
+      mat = scipy.io.loadmat(name)
+      #print(mat.keys())
+
+      if province == "Gauteng":
+         veg = mat['veg_gauteng']
+         bwt = mat['bwt_gauteng']
+    
+      #(time,pixels,band)
+      #(time,pixels,7) - NDVI
+      return veg,bwt
+
+  def concatDataSets(self,veg,bwt):
+      print("Concatting data set...")
+      chan = veg.shape[2]
+      timeslots = veg.shape[0]
+      v_pixels = veg.shape[1]
+      b_pixels = bwt.shape[1] 
+
+      X_concat = np.zeros((v_pixels+b_pixels,timeslots,chan),dtype=float)
+
+      for c in range(chan): 
+	Xv = veg[:,:,c].T
+        Xb = bwt[:,:,c].T
+        #print(Xv.shape)
+        #print(Xb.shape)
+
+        X_concat[:,:,c] = np.vstack((Xv,Xb))
+        #print (X.shape)
+
+      yv = np.ones((v_pixels,1),dtype=int)
+      yb = np.zeros((b_pixels,1),dtype=int)
+      y = np.vstack((yv,yb))[:,0]
+ 
+      return X_concat,y
+  
+       
+if __name__ == "__main__":
+   m = MODIS()
+   veg,bwt = m.loadDataSet(name="Gauteng_nochange.mat",province="Gauteng")
+   X,y = m.concatDataSets(veg,bwt)
+   print(X.shape)
+   print(y.shape)
+
+   
+   
+      
+   
