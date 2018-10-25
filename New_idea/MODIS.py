443537024fa344d8d5392e74ba00151c1d657d98
trienko <trienkog@gmail.com>
2018-10-25 15:03:26 +0200

    started looking into validation curves

--- New_idea/MODIS.py
+++ New_idea/MODIS.py
@@ -8,6 +8,12 @@ from scipy.optimize import leastsq
 from matplotlib.patches import Ellipse
 from termcolor import colored
 from sklearn import mixture
+from sklearn.datasets import load_digits
+from sklearn.svm import SVC
+from sklearn.model_selection import validation_curve
+
+
+
 
 M_LENGTH = 45
 
@@ -102,10 +108,63 @@ class MODIS():
           print(X_model[key].shape)
           print(y_model[key].shape)
 
-      plt.plot(y_model[0])
-      plt.show()
-        
-      
+      #plt.plot(y_model[0])
+      #plt.show()
+
+      return X_model,y_model
+
+  def validtionCurveTest(self,X_model,y_model,X_old,y_old):
+       from sklearn.linear_model import LogisticRegression
+       '''
+       for t in range(45):
+           X = X_model[t]
+           y = y_model[t]
+
+           c = ["r","b"]
+           c2= ["r","b"]
+
+
+           plt.plot(X[y==0,1],X[y==0,5],c[0]+"o",alpha=0.1)
+           plt.plot(X[y==1,1],X[y==1,5],c[1]+"o",alpha=0.1)
+           plt.plot(X_old[y_old==0,t+45,1],X_old[y_old==0,t+45,5],c2[0]+"s",alpha=0.5)
+           plt.plot(X_old[y_old==1,t+45,1],X_old[y_old==1,t+45,5],c2[1]+"s",alpha=0.5)
+           plt.title(str(t))
+           plt.show()
+        '''
+
+       X = X_model[12]
+       y = y_model[12]
+
+       bands = np.array([False,True,False,False,False,True,False,False])
+       
+       X = X[:,bands]
+
+       param_range = np.logspace(-10, 10, 10)
+       train_scores, test_scores = validation_curve(LogisticRegression(), X, y, param_name="C", param_range=param_range,
+       cv=10, scoring="accuracy", n_jobs=1)
+       train_scores_mean = np.mean(train_scores, axis=1)
+       train_scores_std = np.std(train_scores, axis=1)
+       test_scores_mean = np.mean(test_scores, axis=1)
+       test_scores_std = np.std(test_scores, axis=1)
+
+       plt.title("Validation Curve with Logistic Regression")
+       plt.xlabel("$\gamma$")
+       plt.ylabel("Score")
+       plt.ylim(0.0, 1.1)
+       lw = 2
+       plt.semilogx(param_range, train_scores_mean, label="Training score",
+              color="darkorange", lw=lw)
+       plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                  train_scores_mean + train_scores_std, alpha=0.2,
+                  color="darkorange", lw=lw)
+       plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+              color="navy", lw=lw)
+       plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                  test_scores_mean + test_scores_std, alpha=0.2,
+                  color="navy", lw=lw)
+       plt.legend(loc="best")
+       plt.show()
+
 if __name__ == "__main__":
    m = MODIS()
    veg,bwt = m.loadDataSet(name="Gauteng_nochange.mat",province="Gauteng")
@@ -113,7 +172,10 @@ if __name__ == "__main__":
    #print(X.shape)
    #print(y.shape)
    
-   m.createDictionary(X,y)
+   Xdic,ydic = m.createDictionary(X,y)
+
+   m.validtionCurveTest(Xdic,ydic,X,y)
+
    
    
       
