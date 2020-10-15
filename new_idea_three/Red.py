09f0d4f43f2375aa177d97995ab72142545f7211
Trienko Grobler <trienkog@gmail.com>
2020-10-15 15:53:34 +0200

    added experiments

--- new_idea_three/Red.py
+++ new_idea_three/Red.py
@@ -66,7 +66,7 @@ class RedClass():
       model = keras.models.Sequential()
       model.add(keras.layers.Flatten(input_shape=[368,7]))
       #model.add(keras.layers.Dense(368,activation="relu"))
-      model.add(keras.layers.Dense(100,activation="relu",kernel_initializer="he_normal"))
+      model.add(keras.layers.Dense(45,activation="relu",kernel_initializer="he_normal",kernel_regularizer=keras.regularizers.l2(1e-3)))
       model.add(keras.layers.Dense(1,activation="sigmoid"))
       model.summary() 
 
@@ -74,7 +74,7 @@ class RedClass():
       model.compile(loss="binary_crossentropy",optimizer="sgd",metrics=["accuracy"])
 
       #FIT
-      history = model.fit(X_train,y_train,epochs=10,batch_size=1,validation_data=(X_valid,y_valid))
+      history = model.fit(X_train,y_train,epochs=30,batch_size=5,validation_data=(X_valid,y_valid))
 
   def exp2(self,X_train,X_valid,y_train,y_valid):
 
@@ -105,34 +105,148 @@ class RedClass():
 
   def exp5(self,X_train,X_valid,y_train,y_valid):
 
+      idx = np.random.randint(2,size=X_valid.shape[0])
+      X_test = X_valid[idx==0,:,:]
+      y_test = y_valid[idx==0]
+
+      X_valid = X_valid[idx==1,:,:]
+      y_valid = y_valid[idx==1]
+
+      print(X_test.shape)
+      print(X_valid.shape)
+
       #Adding an extra dimension to use convolution layer
       X_train = X_train.reshape((X_train.shape[0],X_train.shape[1],X_train.shape[2],1))
       X_valid = X_valid.reshape((X_valid.shape[0],X_valid.shape[1],X_valid.shape[2],1))
+      X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],X_test.shape[2],1))
+
 
       #BUILDING THE MODEL
       model = keras.models.Sequential()
-      model.add(keras.layers.Conv2D(1,(1,7),activation="relu",input_shape=[368,7,1],padding="valid",kernel_initializer="he_normal"))
+      #model.add(keras.layers.experimental.preprocessing.Normalization(input_shape=[368,7,1]))
+      #model.add(keras.layers.BatchNormalization(input_shape=[368,7,1]))
+      model.add(keras.layers.Conv2D(1,(1,7),activation="tanh",input_shape=[368,7,1],padding="valid",kernel_initializer="glorot_normal"))
       model.add(tf.keras.layers.Reshape((368, 1), input_shape=(368,1,1)))
-      model.add(tf.keras.layers.Conv1D(1,45,activation="relu",strides=45,padding="valid",kernel_initializer="he_normal"))
+      #model.add(keras.layers.BatchNormalization())
+      model.add(tf.keras.layers.Conv1D(1,45,activation="tanh",strides=45,padding="valid",kernel_initializer="glorot_normal"))
       model.add(keras.layers.Flatten())
+      #model.add(keras.layers.BatchNormalization())
       #model.add(keras.layers.Dense(368,activation="relu"))
-      model.add(keras.layers.Dense(8,activation="relu",kernel_initializer="he_normal"))
+      model.add(keras.layers.Dense(8,activation="tanh",kernel_initializer="glorot_normal"))
+      #model.add(keras.layers.BatchNormalization())
       model.add(keras.layers.Dense(1,activation="sigmoid"))
       model.summary() 
       #optimizers.SGD(lr=10e-4)
       #COMPILE
-      model.compile(loss="binary_crossentropy",optimizer=keras.optimizers.Adam(lr=1e-3),metrics=["accuracy"])
-
+      model.compile(loss="binary_crossentropy",optimizer=keras.optimizers.Adam(lr=1e-2),metrics=["accuracy"])
+          
       
 
       #FIT
-      history = model.fit(X_train,y_train,epochs=10,batch_size=1,validation_data=(X_valid,y_valid))
+      history = model.fit(X_train,y_train,epochs=30,batch_size=10,validation_data=(X_valid,y_valid))
 
+      model.evaluate(X_test,y_test,batch_size=1)
+
+      #w,b = model.layers[0].get_weights()
+      #print(w[0,:,0,0])
+      #print(b)
+
+  def denseModel(self,X_train,X_valid,y_train,y_valid,lr=1e-2,r=1e-3,e=10,bs=10):
+
+      #CREATE TEST SET
+      ################# 
+      idx = np.random.randint(2,size=X_valid.shape[0])
+      X_test = X_valid[idx==0,:,:]
+      y_test = y_valid[idx==0]
+
+      X_valid = X_valid[idx==1,:,:]
+      y_valid = y_valid[idx==1]
+      #################
+
+      #BUILDING THE MODEL
+      model = keras.models.Sequential()
+      model.add(keras.layers.Flatten(input_shape=[368,7]))
+      #model.add(keras.layers.Dense(2,activation="relu",kernel_initializer="he_normal",kernel_regularizer=keras.regularizers.l2(r)))
+      model.add(keras.layers.Dense(1,activation="sigmoid",kernel_regularizer=keras.regularizers.l2(r)))
+      model.summary() 
+
+      #COMPILE
+      model.compile(loss="binary_crossentropy",optimizer=keras.optimizers.Adam(lr=lr),metrics=["accuracy"])
+
+      #FIT
+      history = model.fit(X_train,y_train,epochs=e,batch_size=bs,validation_data=(X_valid,y_valid))
+
+      #EVALUATE
+      model.evaluate(X_test,y_test,batch_size=1)
+      y_pred = np.zeros(y_test.shape,dtype=int)
+      y_pred[model.predict(X_test).flatten()>=0.5]=1
+      cm=confusion_matrix(y_test,y_pred)
+      print(cm)
+
+      return cm 
+
+  def parsimonuous_TCN(self,X_train,X_valid,y_train,y_valid,lr=1e-2,r=1e-3,e=10,bs=10):
+      
+      #CREATE TEST SET
+      ################# 
+      idx = np.random.randint(2,size=X_valid.shape[0])
+      X_test = X_valid[idx==0,:,:]
+      y_test = y_valid[idx==0]
+
+      X_valid = X_valid[idx==1,:,:]
+      y_valid = y_valid[idx==1]
+      #################
+
+      #Adding an extra dimension to use convolution layer
+      X_train = X_train.reshape((X_train.shape[0],X_train.shape[1],X_train.shape[2],1))
+      X_valid = X_valid.reshape((X_valid.shape[0],X_valid.shape[1],X_valid.shape[2],1))
+      X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],X_test.shape[2],1))
+
+
+      #BUILDING THE MODEL USING THE SEQ API
+      #################
+      model = keras.models.Sequential()
+      model.add(keras.layers.Conv2D(1,(1,7),activation="tanh",input_shape=[368,7,1],padding="valid",kernel_initializer="glorot_normal",kernel_regularizer=keras.regularizers.l2(r)))
+      model.add(tf.keras.layers.Reshape((368, 1), input_shape=(368,1,1)))
+      model.add(tf.keras.layers.Conv1D(1,45,activation="tanh",strides=45,padding="valid",kernel_initializer="glorot_normal",kernel_regularizer=keras.regularizers.l2(r)))
+      model.add(keras.layers.Flatten())
+      model.add(keras.layers.Dense(8,activation="tanh",kernel_initializer="glorot_normal",kernel_regularizer=keras.regularizers.l2(r)))
+      model.add(keras.layers.Dense(1,activation="sigmoid"))
+      #model.summary() 
+      #################    
+      
+
+      #COMPILE AND FIT
+      model.compile(loss="binary_crossentropy",optimizer=keras.optimizers.Adam(lr=lr),metrics=["accuracy"])
+      history = model.fit(X_train,y_train,epochs=e,batch_size=bs,validation_data=(X_valid,y_valid))
+
+      #EVALUATE
+      model.evaluate(X_test,y_test,batch_size=1)
+      y_pred = np.zeros(y_test.shape,dtype=int)
+      y_pred[model.predict(X_test).flatten()>=0.5]=1
+      cm=confusion_matrix(y_test,y_pred)
+      print(cm) 
+
+      #SAVING THE WEIGHTS
+      #idx = np.zeros((11,45),dtype=int)
+      #idx[0,:7] = 1
+      #idx[1,:] = 1
+      #idx[2:10,:8] = 1
+      #idx[10,:8] = 1
+
+      weights = np.zeros((11,45),dtype=float)
       w,b = model.layers[0].get_weights()
-      print(w[0,:,0,0])
-      print(b)
-
-
+      weights[0,:7] = (w[0,:,0,0]-np.amin(w[0,:,0,0]))/(np.amax(w[0,:,0,0])-np.amin(w[0,:,0,0]))
+      w,b = model.layers[2].get_weights()
+      weights[1,:] = (w[:,0,0]-np.amin(w[:,0,0]))/(np.amax(w[:,0,0])-np.amin(w[:,0,0])) 
+      w,b = model.layers[4].get_weights()
+      weights[2:10,:8] = (w-np.amin(w))/(np.amax(w)-np.amin(w)) 
+      #print(w.shape) 
+      w,b = model.layers[5].get_weights()
+      weights[10,:8] = (w[:,0]-np.amin(w[:,0]))/(np.amax(w[:,0])-np.amin(w[:,0])) 
+      
+      return cm,weights
+            
 
   def exp3(self,X_train,X_valid,y_train,y_valid):
 
@@ -336,9 +450,14 @@ def set_global_determinism(seed=10, fast_n_close=False):
 
    
 if __name__ == "__main__":
-   #np.random.seed(20)
-   set_global_determinism()         
+   
+   seed = 10
+   ex = 50
+
+   set_global_determinism(seed)         
    red_object = RedClass() 
+   filename = 'results'+str(seed)+".pkl"
+   outfile = open(filename,'wb')
      
    #LOADING DATASET
    veg,bwt = red_object.loadDataSet()
@@ -348,31 +467,42 @@ if __name__ == "__main__":
    X,y = red_object.concatDataSets(veg,bwt)
    plt.plot(X[0,:,0]/10000.0,"b") 
 
-   #X = red_object.filter_data(X,25)
-
-   #print(X.shape)
+   #REMOVE NDVI AND RESCALE
    X_ndvi = X[:,:,-1]
    X = X[:,:,:-1]/10000.0
 
-   plt.plot(X[0,:,0],"r")
-   plt.show()
+   tcn_weights = np.zeros((ex,11,45),dtype=float)
+   tcn_cm = np.zeros((ex,2,2),dtype=int)
+   dense_cm = np.zeros((ex,2,2),dtype=int)
 
-   idx = np.random.randint(2,size=X.shape[0])
+   for e in range(ex):
+  
+       #CONSTRUCT TRAINING/VALIDATION SET   
+       idx = np.random.randint(2,size=X.shape[0])
+       X_train, X_valid, y_train, y_valid = X[idx==0,:,:],X[idx==1,:,:],y[idx==0],y[idx==1]
 
-   #print(idx)
+       #TCN EXPERIMENT
+       print("TCN: "+str(e))
+       tcn_cm[e,:,:],tcn_weights[e,:,:] = red_object.parsimonuous_TCN(X_train,X_valid,y_train,y_valid)
+       print("DENSE: "+str(e))
+       dense_cm[e,:,:] = red_object.denseModel(X_train,X_valid,y_train,y_valid)  
 
-   X_train, X_valid, y_train, y_valid = X[idx==0,:,:],X[idx==1,:,:],y[idx==0],y[idx==1]
+   pickle.dump(tcn_cm,outfile)
+   pickle.dump(tcn_weights,outfile)
+   pickle.dump(dense_cm,outfile)
+   outfile.close() 
 
-   print(X_train.shape)
+   #print(X_train.shape)
    #print(X_valid.shape)
    
-   print(tf.__version__)
-   for k in range(1):
-       print(k)
-       red_object.exp1(X_train,X_valid,y_train,y_valid) 
+   #print(tf.__version__)
+   #for k in range(1):
+   #    print(k)
+       #red_object.exp1(X_train,X_valid,y_train,y_valid) 
    #red_object.exp2(X_train,X_valid,y_train,y_valid) 
    #red_object.exp4(X_train,X_valid,y_train,y_valid)
-       red_object.exp5(X_train,X_valid,y_train,y_valid) 
+   #    red_object.parsimonuous_TCN(X_train,X_valid,y_train,y_valid) 
+   #    red_object.exp2(X_train,X_valid,y_train,y_valid) 
    #X_new = red_object.process_data(X,np.array([0.7249732,0.16765448,0.6030529,0.41907814,0.5606056,-0.29389688,-0.7113738]),-0.10607813)
    #X_new = red_object.process_data2(X,np.array([0.92509115,-0.31727937,0.23246963,0.24132209,-0.17650443,0.08887789,-0.35802707]),0.04901218,np.array([-0.38472643,-0.5096011,0.22391927,0.11414693,-0.14565101,0.39224625,0.01539364]),0.03469806)
    #X_new = red_object.process_data2(X,np.array([0.92509115,-0.31727937,0.23246963,0.24132209,-0.17650443,0.08887789,-0.35802707]),0.04901218,np.array([-0.38472643,-0.5096011,0.22391927,0.11414693,-0.14565101,0.39224625,0.01539364]),0.03469806)
